22dcc1db831184ea1c238efd219d6b7cf116424f
Luke Prescott <luke.prescott@presport.us>
2019-11-06 12:59:08 -0500

    Spring Boot Intialzr in Server directory

---
+++ server/src/test/java/com/jahnelgroup/todolist/TodoListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jahnelgroup.todolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoListApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
