683b8eed41413e2f9a5db224acea971c5736fec9
Luke Prescott <luke.prescott@presport.us>
2019-11-06 11:55:31 -0500

    Removed unnecessary use of title set

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,12 +7,4 @@ import { Title } from '@angular/platform-browser';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'Todo List Challenge';
-  public constructor(private titleService: Title ) {
-    this.setTitle(this.title);
-  }
-
-  public setTitle( newTitle: string): void {
-    this.titleService.setTitle( newTitle );
-  }
 }
