c80333a234d9d012c9a26b8797a76487d10fb3ee
Luke Prescott <luke.prescott@presport.us>
2019-11-08 11:46:26 -0500

    Added POST, PUT, DELETE unit testing

--- server/src/test/java/com/jahnelgroup/todolist/TodoListApplicationTests.java
+++ server/src/test/java/com/jahnelgroup/todolist/TodoListApplicationTests.java
@@ -1,5 +1,6 @@
 package com.jahnelgroup.todolist;
 
+import com.jahnelgroup.todolist.todo.model.Todo;
 import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
@@ -7,11 +8,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.web.client.HttpClientErrorException;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = TodoListApplication.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -27,7 +26,7 @@ class TodoListApplicationTests {
      * This function creates the full web path of the todos api.
      * @return The web url of the todos api.
      */
-    private String getRootUrl() {
+    private String getTodosURL() {
         return "http://localhost:" + port + "/todos";
     }
 
@@ -44,9 +43,68 @@ class TodoListApplicationTests {
         HttpHeaders headers = new HttpHeaders();
         HttpEntity<String> entity = new HttpEntity<String>(null, headers);
 
-        ResponseEntity<String> response = testRestTemplate.exchange(getRootUrl(),
+        ResponseEntity<String> response = testRestTemplate.exchange(getTodosURL(),
                 HttpMethod.GET, entity, String.class);
 
         Assert.assertNotNull(response.getBody());
+    }
+
+    /**
+     * Here we test if we can fetch a single todo using its id
+     */
+    @Test
+    public void testGetTodoById() {
+        Todo todo = testRestTemplate.getForObject(getTodosURL() + "/1", Todo.class);
+        System.out.println(todo.getTitle());
+        Assert.assertNotNull(todo);
+    }
+
+    /**
+     * Here we test if we can create a todo using the POST method
+     */
+    @Test
+    public void testCreateTodo() {
+        Todo todo = new Todo();
+        todo.setTitle("Test Todo");
+        todo.setCompleted(false);
+
+        ResponseEntity<Todo> postResponse = testRestTemplate.postForEntity(getTodosURL(), todo, Todo.class);
+        Assert.assertNotNull(postResponse);
+        Assert.assertNotNull(postResponse.getBody());
+    }
+
+    /**
+     * Here we test that we can update a todo's information using the PUT method
+     */
+    @Test
+    public void testUpdateTodo() {
+        int id = 1;
+        Todo todoNew = testRestTemplate.getForObject(getTodosURL() + "/" + id, Todo.class);
+        todoNew.setTitle("Test Todo");
+        todoNew.setCompleted(true);
+
+        testRestTemplate.put(getTodosURL() + "/" + id, todoNew);
+
+        Todo updatedTodo = testRestTemplate.getForObject(getTodosURL() + "/" + id, Todo.class);
+        Assert.assertNotNull(updatedTodo);
+    }
+
+    /**
+     * Here we test that we can delete a todo by using the DELETE method,
+     * then we verify that it no longer exists in the database
+     */
+    @Test
+    public void testDeleteTodo() {
+        int id = 2;
+        Todo todo = testRestTemplate.getForObject(getTodosURL() + "/" + id, Todo.class);
+        Assert.assertNotNull(todo);
+
+        testRestTemplate.delete(getTodosURL() + "/" + id);
+
+        try {
+            todo = testRestTemplate.getForObject(getTodosURL() + "/" + id, Todo.class);
+        } catch (final HttpClientErrorException e) {
+            Assert.assertEquals(e.getStatusCode(), HttpStatus.NOT_FOUND);
+        }
     }
 }
