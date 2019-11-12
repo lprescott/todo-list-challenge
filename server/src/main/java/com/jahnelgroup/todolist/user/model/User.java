f9d6c4f4a782a37a357921ea820f7f4674824823
Luke Prescott <luke.prescott@presport.us>
2019-11-12 13:00:50 -0500

    Created User model (Java)

---
+++ server/src/main/java/com/jahnelgroup/todolist/user/model/User.java
@@ -0,0 +1,24 @@
+package com.jahnelgroup.todolist.user.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NonNull
+    private String username;
+}
