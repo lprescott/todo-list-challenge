937cb5b5daab930dc0c9bdde0b4815ffa4862052
Luke Prescott <luke.prescott@presport.us>
2019-11-12 12:58:31 -0500

    Added login page and styling

--- web/src/app/components/login/login.component.ts
+++ web/src/app/components/login/login.component.ts
@@ -6,10 +6,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
+  username: string;
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  onSubmit() {
+    console.log('An attempt to login with: \'' + this.username + '\' was received.');
+  }
 }
