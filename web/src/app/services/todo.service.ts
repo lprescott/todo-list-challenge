8a2696bb13c894854a2117af68bc8dd2aa4730e2
Luke Prescott <luke.prescott@presport.us>
2019-11-13 09:40:32 -0500

    Updated and added todoList and todo service

--- web/src/app/services/todo.service.ts
+++ web/src/app/services/todo.service.ts
@@ -18,49 +18,14 @@ const httpOptions = {
 })
 export class TodoService {
 
-  // backend api's controller path
-  todosUrl = '/todos';
-
   // passing an HttpClient through the constructor allows its access
   constructor(private http: HttpClient) { }
 
-  // get todos with the limit 0
-  // observables provide support for passing messages between publishers and subscribers
-  getTodos(): Observable<Todo[]> {
-    return this.http.get<Todo[]>(this.todosUrl)
-        .pipe(catchError(this.handleError));
-
-  }
-
-  // delete the todo with the passed id
-  deleteTodo(todo: Todo): Observable<Todo> {
-    const url = `${this.todosUrl}/${todo.id}`;
-    return this.http.delete<Todo>(url, httpOptions)
-      .pipe(catchError(this.handleError));
-  }
-
-  // add the passed observable
-  addTodo(todo: Todo): Observable<Todo> {
-    return this.http.post<Todo>(this.todosUrl, todo, httpOptions)
-      .pipe(catchError(this.handleError));
-  }
-
-  // toggle completion of the passed observable
-  toggleCompleted(todo: Todo): Observable<any> {
-    const url = `${this.todosUrl}/${todo.id}`;
-    return this.http.put(url, todo, httpOptions)
-      .pipe(catchError(this.handleError));
-  }
-
-  // update the title of existing todo
-  updateTodo(todo: Todo): Observable<Todo> {
-    const url = `${this.todosUrl}/${todo.id}`;
-    return this.http.put<Todo>(url, todo, httpOptions)
-      .pipe(catchError(this.handleError));
-  }
+  // backend api's controller path
+  todosUrl = '/todos';
 
   // logs an error depending on the type, and throws an error
-  private handleError(error: HttpErrorResponse) {
+  public static handleError(error: HttpErrorResponse) {
     if (error.error instanceof ErrorEvent) {
       console.error('An error occurred:', error.error.message);
     } else {
@@ -68,5 +33,40 @@ export class TodoService {
     }
 
     return throwError('Something bad happened; please try again later.');
+  }
+
+  // get todos with the limit 0
+  // observables provide support for passing messages between publishers and subscribers
+  getTodos(): Observable<Todo[]> {
+    return this.http.get<Todo[]>(this.todosUrl)
+        .pipe(catchError(TodoService.handleError));
+
+  }
+
+  // delete the todo with the passed id
+  deleteTodo(todo: Todo): Observable<Todo> {
+    const url = `${this.todosUrl}/${todo.id}`;
+    return this.http.delete<Todo>(url, httpOptions)
+      .pipe(catchError(TodoService.handleError));
+  }
+
+  // add the passed observable
+  addTodo(todo: Todo): Observable<Todo> {
+    return this.http.post<Todo>(this.todosUrl, todo, httpOptions)
+      .pipe(catchError(TodoService.handleError));
+  }
+
+  // toggle completion of the passed observable
+  toggleCompleted(todo: Todo): Observable<any> {
+    const url = `${this.todosUrl}/${todo.id}`;
+    return this.http.put(url, todo, httpOptions)
+      .pipe(catchError(TodoService.handleError));
+  }
+
+  // update the title of existing todo
+  updateTodo(todo: Todo): Observable<Todo> {
+    const url = `${this.todosUrl}/${todo.id}`;
+    return this.http.put<Todo>(url, todo, httpOptions)
+      .pipe(catchError(TodoService.handleError));
   }
 }
---
+++ web/src/app/services/todolist.service.ts
@@ -0,0 +1,50 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+import { catchError } from 'rxjs/operators';
+import {TodoList} from '../models/TodoList';
+import {TodoService} from './todo.service';
+
+// this const outlines the used http content-type in a JSON format
+// used for delete, post, and put
+const httpOptions = {
+  headers: new HttpHeaders({
+    'Content-Type': 'application/json'
+  })
+};
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TodolistService {
+
+  // backend api's controller path
+  todoListsUrl = '/lists';
+
+  // passing an HttpClient through the constructor allows its access
+  constructor(private http: HttpClient) { }
+
+  getTodoLists(): Observable<TodoList[]> {
+    return this.http.get<TodoList[]>(this.todoListsUrl)
+        .pipe(catchError(TodoService.handleError));
+
+  }
+
+  deleteTodoList(todoList: TodoList): Observable<TodoList> {
+    const url = `${this.todoListsUrl}/${todoList.id}`;
+    return this.http.delete<TodoList>(url, httpOptions)
+        .pipe(catchError(TodoService.handleError));
+  }
+
+  addTodoList(todoList: TodoList): Observable<TodoList> {
+    return this.http.post<TodoList>(this.todoListsUrl, todoList, httpOptions)
+        .pipe(catchError(TodoService.handleError));
+  }
+
+  updateTodoList(todoList: TodoList): Observable<TodoList> {
+    const url = `${this.todoListsUrl}/${todoList.id}`;
+    return this.http.put<TodoList>(url, todoList, httpOptions)
+        .pipe(catchError(TodoService.handleError));
+  }
+}
