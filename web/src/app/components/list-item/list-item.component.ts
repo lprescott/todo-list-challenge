3effac7b66154835d48dd4af781f53e4050c3a3c
Luke Prescott <luke.prescott@presport.us>
2019-11-14 09:41:10 -0500

    Added prettier package, Added confirmation on list delete

--- web/src/app/components/list-item/list-item.component.ts
+++ web/src/app/components/list-item/list-item.component.ts
@@ -1,9 +1,10 @@
-import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
-import {TodoList} from '../../models/TodoList';
-import {TodolistService} from '../../services/todolist.service';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { TodoList } from '../../models/TodoList';
+import { TodolistService } from '../../services/todolist.service';
 import { Router } from '@angular/router';
 import { faList } from '@fortawesome/free-solid-svg-icons';
 import swal from 'sweetalert2';
+import { TodoService } from '../../services/todo.service';
 
 @Component({
   selector: 'app-list-item',
@@ -11,8 +12,11 @@ import swal from 'sweetalert2';
   styleUrls: ['./list-item.component.scss']
 })
 export class ListItemComponent implements OnInit {
-
-  constructor(private router: Router, private todoListService: TodolistService) { }
+  constructor(
+    private router: Router,
+    private todoListService: TodolistService,
+    private todoService: TodoService
+  ) {}
 
   @Input() list: TodoList;
   @Output() deleteList: EventEmitter<TodoList> = new EventEmitter<TodoList>();
@@ -20,11 +24,9 @@ export class ListItemComponent implements OnInit {
 
   faList = faList;
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
   onSave(list: TodoList, newName: string) {
-
     if (this.list.name === newName || newName.length === 0) {
       console.log('\'' + this.list.name + '\' not updated');
       return;
@@ -40,27 +42,43 @@ export class ListItemComponent implements OnInit {
 
   // emits the function deleteTodo to todo-list component and logs
   onDelete(list: TodoList) {
-    swal.fire({
-      title: 'Are you sure?',
-      text: 'You won\'t be able to revert this!',
-      icon: 'warning',
-      showCancelButton: true,
-      confirmButtonColor: '#4cabff',
-      cancelButtonColor: '#FF605C',
-      confirmButtonText: 'Yes, delete it!'
-    }).then((result) => {
-      if (result.value) {
+    // get id of list
+    const id = list.id;
+
+    let count = 0;
+    this.todoService.getTodos().subscribe(todos => {
+      // get count of items in list
+      todos = todos.filter(td => td.todoList.id === id);
+      count = todos.length;
+
+      // confirmation only on list with tasks
+      if (count > 0) {
+        swal
+          .fire({
+            title: 'Are you sure?',
+            text: 'You won\'t be able to revert this!',
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#4cabff',
+            cancelButtonColor: '#FF605C',
+            confirmButtonText: 'Yes, delete it!'
+          })
+          .then(result => {
+            if (result.value) {
+              this.deleteList.emit(list);
+              swal.fire('Deleted!', 'Your list has been deleted.', 'success');
+            }
+          });
+      } else {
         this.deleteList.emit(list);
-        swal.fire(
-            'Deleted!',
-            'Your file has been deleted.',
-            'success'
-        );
+        swal.fire('Deleted!', 'Your list has been deleted.', 'success');
       }
     });
   }
 
   onRoute(list: TodoList) {
-    this.router.navigateByUrl('/list/' + list.id).then(r => console.log('Successfully redirected to /list/' + list.id));
+    this.router
+      .navigateByUrl('/list/' + list.id)
+      .then(r => console.log('Successfully redirected to /list/' + list.id));
   }
 }
