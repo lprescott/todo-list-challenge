fab0e718385e3e6abb0d89c60c56e91b6bce3f10
Luke Prescott <luke.prescott@presport.us>
2019-11-15 12:41:44 -0500

    Started on User and List Service Unit Testing

--- web/src/app/services/list/todolist.service.spec.ts
+++ web/src/app/services/list/todolist.service.spec.ts
@@ -1,15 +1,42 @@
 import { TestBed } from '@angular/core/testing';
 
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { BrowserModule } from '@angular/platform-browser';
 import { TodolistService } from './todolist.service';
-import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { TodoService } from '../todo/todo.service';
+import { TodoList } from '../../models/TodoList';
+import { HttpClientModule } from '@angular/common/http';
+
 
 describe('TodolistService', () => {
-  beforeEach(() => TestBed.configureTestingModule({
-    imports: [HttpClientTestingModule]
-  }));
+
+  // create a mock httpMock object and service used for testing
+  let httpMock: HttpTestingController;
+  let service: TodolistService;
+
+  // create a list for testing
+  const firstList = new TodoList();
+  firstList.id = 1;
+  firstList.name = 'Test Todo List';
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [
+        BrowserModule,
+        HttpClientModule,
+        HttpClientTestingModule
+      ],
+      providers: [
+        TodolistService
+      ]
+    });
+
+    httpMock = TestBed.get(HttpTestingController);
+    service = TestBed.get(TodolistService);
+  });
 
   it('should be created', () => {
-    const service: TodolistService = TestBed.get(TodolistService);
+    service = TestBed.get(TodolistService);
     expect(service).toBeTruthy();
   });
 });
--- web/src/app/services/user/user.service.spec.ts
+++ web/src/app/services/user/user.service.spec.ts
@@ -1,15 +1,41 @@
 import { TestBed } from '@angular/core/testing';
 
 import { UserService } from './user.service';
-import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { BrowserModule } from '@angular/platform-browser';
+import { TodolistService } from '../list/todolist.service';
+import { User } from '../../models/User';
+import { HttpClientModule } from '@angular/common/http';
 
 describe('UserService', () => {
-  beforeEach(() => TestBed.configureTestingModule({
-    imports: [HttpClientTestingModule]
-  }));
+
+  // create a mock httpMock object and service used for testing
+  let httpMock: HttpTestingController;
+  let service: TodolistService;
+
+  // create a list for testing
+  const firstUser = new User();
+  firstUser.id = 1;
+  firstUser.name = 'Test User';
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [
+        BrowserModule,
+        HttpClientModule,
+        HttpClientTestingModule
+      ],
+      providers: [
+        UserService
+      ]
+    });
+
+    httpMock = TestBed.get(HttpTestingController);
+    service = TestBed.get(UserService);
+  });
 
   it('should be created', () => {
-    const service: UserService = TestBed.get(UserService);
+    service = TestBed.get(UserService);
     expect(service).toBeTruthy();
   });
 });
