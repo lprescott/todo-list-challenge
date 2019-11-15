4a8f3a9a664e6180fbb57f5ff2f0ebeb597ed3c8
Luke Prescott <luke.prescott@presport.us>
2019-11-15 11:51:17 -0500

    Karma Unit Test TodoList 2/9

--- web/src/app/components/list/list-list/list-list.component.spec.ts
+++ web/src/app/components/list/list-list/list-list.component.spec.ts
@@ -1,6 +1,12 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ListListComponent } from './list-list.component';
+import { ListNewComponent } from '../list-new/list-new.component';
+import { ListItemComponent } from '../list-item/list-item.component';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { RouterTestingModule } from '@angular/router/testing';
 
 describe('ListListComponent', () => {
   let component: ListListComponent;
@@ -8,7 +14,8 @@ describe('ListListComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ListListComponent ]
+      declarations: [ ListListComponent, ListNewComponent, ListItemComponent ],
+      imports: [ FormsModule, HttpClientModule, FontAwesomeModule, RouterTestingModule ]
     })
     .compileComponents();
   }));
