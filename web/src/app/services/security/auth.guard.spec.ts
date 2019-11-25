60f7f22c910696844cf6438a8903d90b65d3c9bc
Luke Prescott <luke.prescott@presport.us>
2019-11-25 13:49:45 -0500

    Fixed auth guard unit test

--- web/src/app/services/security/auth.guard.spec.ts
+++ web/src/app/services/security/auth.guard.spec.ts
@@ -1,10 +1,14 @@
 import { TestBed, async, inject } from '@angular/core/testing';
 
 import { AuthGuard } from './auth.guard';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 describe('AuthGuard', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
+      imports: [BrowserModule, HttpClientModule, HttpClientTestingModule],
       providers: [AuthGuard]
     });
   });
