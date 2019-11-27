5b988dd002ab39cc03c8c24ba8d669d5dad4afa2
Luke Prescott <lukep@nycap.rr.com>
2019-11-27 16:17:28 -0500

    Added necessary scripts

---
+++ sql/grant_permission.sql
@@ -0,0 +1,2 @@
+create user 'test_user'@'localhost' identified by 'test_code';
+grant all privileges on test_db.* to 'test_user'@'localhost';
--- sql/init_users.sql
+++ sql/init_users.sql
@@ -1,3 +1,4 @@
+use test_db;
 insert into user (user_id, active, password, roles, username) values (1, 1, 'pass', 'ROLE_USER', 'user1');
 insert into user (user_id, active, password, roles, username) values (2, 1, 'pass', 'ROLE_USER', 'user2');
 insert into user (user_id, active, password, roles, username) values (3, 1, 'pass', 'ROLE_USER', 'user3');
