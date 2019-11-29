48cc27407f3cde28a21841caa35fc5314d044a05
Luke Prescott <luke.prescott@presport.us>
2019-11-29 09:49:44 -0500

    Customized login page, simple angular routing

--- web/src/app/app-routing.module.ts
+++ web/src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {UserComponent} from './components/user/user.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/user', pathMatch: 'full' },
+  { path: 'user', component: UserComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- web/src/app/app.module.ts
+++ web/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UserComponent } from './components/user/user.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserComponent
   ],
   imports: [
     BrowserModule,
